991479b929da27929504675ede4a9f48f7caa37c
gavin318 <gavin.ykjhs@gmail.com>
2018-10-11 08:17:42 +0800

    讀出所有測驗

---
+++ resources/views/exam/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app') 
+@section('content')
+    <h1>隨機題庫系統</h1>
+    <div class="list-group">
+        @forelse($exams as $exam)
+            <a href="exam/{{ $exam->id }}" class="list-group-item list-group-item-action">{{ $exam->updated_at->format("Y年m月d日") }} - {{ $exam->title }}</a>
+        @empty
+            <div class="list-group">尚無任何測驗</div>
+        @endforelse        
+    </div>
+@endsection
