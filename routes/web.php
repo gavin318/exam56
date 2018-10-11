c0a5e18b1cf97adc6bf4ac7f9c0e742a3108cea5
gavin318 <gavin.ykjhs@gmail.com>
2018-10-11 09:10:22 +0800

    儲存題目

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/exam/{exam}', 'ExamController@show')->name('exam.show');
 Route::get('/exam', 'ExamController@index')->name('exam.index');
 Route::get('/exam/create', 'ExamController@create')->name('exam.create');
 Route::post('/exam', 'ExamController@store')->name('exam.store');
+Route::post('/topic', 'TopicController@store')->name('topic.store');
