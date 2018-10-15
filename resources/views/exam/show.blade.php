d259e3559cd595854ac44b1818a1109b666b4327
gavin318 <gavin.ykjhs@gmail.com>
2018-10-15 20:54:53 +0800

    加入刪除確認

--- resources/views/exam/show.blade.php
+++ resources/views/exam/show.blade.php
@@ -3,11 +3,7 @@
     <h1 class="text-center">
         {{$exam->title}}
         @can('建立測驗')
-            <form action="{{route('exam.destroy', $exam->id)}}" method="POST" style="display:inline">
-                @csrf
-                @method('delete')
-                <button type="submit"  class="btn btn-danger">刪除</button>
-            </form>
+            <a href="#" class="btn btn-danger btn-del-exam" data-id="{{ $exam->id }}">刪除</a>
             <a href="{{route('exam.edit', $exam->id)}}" class="btn btn-warning">編輯</a>
         @endcan
     </h1>
@@ -62,11 +58,7 @@
         <dl>
             <dt class="h3">
                 @can('建立測驗')
-                    <form action="{{route('topic.destroy', $topic->id)}}"  method="post" style="display:inline">
-                        @csrf
-                        @method('delete')
-                        <button type="submit" class="btn btn-danger">刪除</button>
-                    </form>
+                    <button type="button" class="btn btn-danger btn-del-topic" data-id="{{ $topic->id }}">刪除</button>
                     <a href="{{route('topic.edit', $topic->id)}}" class="btn btn-warning">編輯</a>
                     （{{$topic->ans}}）
                 @endcan
@@ -90,4 +82,58 @@
     <div class="text-center">
         發佈於 {{$exam->created_at->format("Y年m月d日 H:i:s")}} / 最後更新： {{$exam->updated_at->format("Y年m月d日 H:i:s")}}
     </div>
+@endsection
+
+@section('js')
+    <script>
+        $(document).ready(function(){
+            $('.btn-del-topic').click(function(){
+                var topic_id=$(this).data('id');                
+                swal({
+                    title: "確定要刪除題目嗎？",
+                    text: "刪除後該題目就消失救不回來囉！",
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "是！含淚刪除！",
+                    cancelButtonText: "不...別刪",
+                }).then((result) => {
+                    if (result.value) {                        
+                        axios.delete('/topic/' + topic_id)
+                        .then(function(){
+                            return swal("OK！刪掉題目惹！", "該題目已經隨風而逝了...", "success");
+                        }).then(function () {
+                            location.reload();
+                        });
+                    }
+                })
+            });
+
+            // 刪除按鈕點擊事件
+            $('.btn-del-exam').click(function() {
+                // 獲取按鈕上 data-id 屬性的值，也就是編號
+                var id = $(this).data('id');
+                // 調用 sweetalert
+                swal({
+                    title: "確定要刪除測驗嗎？",
+                    text: "刪除後該測驗連同所有題目就消失救不回來囉！",
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "是！含淚刪除！",
+                    cancelButtonText: "不...別刪",
+                }).then((result) => {
+                    if (result.value) {
+                        // 調用刪除介面，用 id 來拼接出請求的 url
+                        axios.delete('/exam/' + id)
+                        .then(function(){
+                            return swal("OK！刪掉測驗惹！", "該測驗所有資料已經隨風而逝了...", "success");
+                        }).then(function () {
+                            location.href='/exam';
+                        });
+                    }
+                });
+            });
+        });
+    </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,10 +12,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.5/sweetalert2.all.js"></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -35,5 +33,6 @@
             </div>
         </main>
     </div>
+    @yield('js')
 </body>
 </html>
