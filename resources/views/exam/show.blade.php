a265955b146cca2a88049a37b279fc00664d9102
gavin318 <gavin.ykjhs@gmail.com>
2018-10-15 20:20:16 +0800

    刪除題目

--- resources/views/exam/show.blade.php
+++ resources/views/exam/show.blade.php
@@ -57,6 +57,11 @@
         <dl>
             <dt class="h3">
                 @can('建立測驗')
+                    <form action="{{route('topic.destroy', $topic->id)}}"  method="post" style="display:inline">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-danger">刪除</button>
+                    </form>
                     <a href="{{route('topic.edit', $topic->id)}}" class="btn btn-warning">編輯</a>
                     （{{$topic->ans}}）
                 @endcan
