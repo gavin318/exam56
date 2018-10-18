b172da0c13b265210ba91ff32d50472a6b64ea7e
gavin318 <gavin.ykjhs@gmail.com>
2018-10-18 16:47:51 +0800

    儲存並計分

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Test;
+use App\Topic;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $content = collect($request->ans)->toJson();
+        $score   = 0;
+        // 取得題數
+        $show_num = collect($request->ans)->count();
+        // 答對題數
+        $right_num = 0;
+        foreach ($request->ans as $topic_id => $ans) {
+            $topic = Topic::find($topic_id);
+            if ($topic->ans == $ans) {
+                $right_num++;
+            }
+        }
+
+        $score = 100 * round($right_num / $show_num, 2);
+
+        $test = Test::create([
+            'content' => $content,
+            'user_id' => $request->user_id,
+            'exam_id' => $request->exam_id,
+            'score'   => $score,
+        ]);
+        return redirect()->route('test.show', $test->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
  */
 Route::pattern('exam', '[0-9]+');
 Route::pattern('topic', '[0-9]+');
+Route::pattern('test', '[0-9]+');
 
 Route::get('/', 'ExamController@index')->name('index');
 
@@ -26,7 +27,11 @@ Route::post('/exam', 'ExamController@store')->name('exam.store');
 Route::get('/exam/{exam}/edit', 'ExamController@edit')->name('exam.edit');
 Route::patch('/exam/{exam}', 'ExamController@update')->name('exam.update');
 Route::delete('/exam/{exam}', 'ExamController@destroy')->name('exam.destroy');
+
 Route::post('/topic', 'TopicController@store')->name('topic.store');
 Route::get('/topic/{topic}/edit', 'TopicController@edit')->name('topic.edit');
 Route::patch('/topic/{topic}', 'TopicController@update')->name('topic.update');
 Route::delete('/topic/{topic}', 'TopicController@destroy')->name('topic.destroy');
+
+Route::post('/test', 'TestController@store')->name('test.store');
+Route::get('/test/{test}', 'TestController@show')->name('test.show');
