d484281dfccd9795a043a0cf54a03ea6fc643f1b
gavin318 <gavin.ykjhs@gmail.com>
2018-11-05 20:07:13 +0800

    使用新北市OpenID登入

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        T301000\LaravelNtpcOpenid\NtpcOpenidServiceProvider::class,
+
     ],
 
     /*
